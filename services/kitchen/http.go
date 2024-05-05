c5eb0e89a7bb2e051174868a6599e5cee3139fa0
elijah <cuiyeqing960904@gmail.com>
2024-05-05 12:46:34 -0400

    building kitchen http

--- services/kitchen/http.go
+++ services/kitchen/http.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"net/http"
+)
+
+type httpServer struct {
+	addr string
+}
+
+func NewHttpServer(addr string) *httpServer {
+	return &httpServer{
+		addr: addr,
+	}
+}
+
+func (s *httpServer) Run() error {
+	router := http.NewServeMux()
+
+	
+	return http.ListenAndServe(s.addr, router)
+}
