c4ba08385d36c764f7850b8be849b552b64fde8e
elijah <cuiyeqing960904@gmail.com>
2024-05-05 08:33:47 -0400

    building http

--- services/orders/http.go
+++ services/orders/http.go
@@ -1,1 +1,15 @@
 package main
+
+type httpServer struct {
+	addr string
+}
+
+func NewHttpServer(addr string) *gRPCServer {
+	return &gRPCServer{
+		addr: addr,
+	}
+}
+
+func (s *httpServer) Run() error {
+
+}
